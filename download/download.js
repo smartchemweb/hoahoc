1c40abe6c16558002fbf49339e7d2cd7afcdc553
smartchemweb <superchem123123@gmail.com>
2025-12-02 11:03:15 +0700

    Add files via upload

--- download/download.js
+++ download/download.js
@@ -1,5 +1,11 @@
 var versions = [
     {
+        version: '2025.2',
+        date: '02-12-2025',
+        name: 'SmartChem2',
+        os: 'Android',
+        app: '../app-versions/smartchem-2025-2.apk'
+    }, {
         version: '2025.1',
         date: '05-11-2025',
         name: 'SmartChem1',
